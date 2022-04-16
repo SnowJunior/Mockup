f6d8ddc9ffb773c4ffeaa79a8f076ca1112f10e8
Junior <onyangogeorge954@gmail.com>
2022-04-16 18:31:14 +0300

    delete card model

--- lib/Presentation/design.dart
+++ lib/Presentation/design.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:interapp/Constants/color.dart';
-import 'package:carousel_slider/carousel_slider.dart';
+
 
 class DesignPage extends StatelessWidget {
   const DesignPage(
@@ -15,142 +15,138 @@ class DesignPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Column(
-      children: [
-        Container(
-          height: size.height * 0.2,
-          decoration: const BoxDecoration(
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(20),
-              bottomRight: Radius.circular(20),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            height: size.height * 0.2,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [kPrimary, kSecondary],
+              ),
             ),
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [kPrimary, kSecondary],
-            ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.only(left: 28.0, top: 48.0),
-            child: Row(
-              children: const [
-                Text(
-                  "Design",
-                  style: TextStyle(
-                    fontSize: 34,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        SizedBox(
-          width: MediaQuery.of(context).size.width,
-          child: Card(
             child: Padding(
-              padding: const EdgeInsets.all(48.0),
-              child: Column(
-                children: [
+              padding: const EdgeInsets.only(left: 28.0, top: 48.0),
+              child: Row(
+                children: const [
                   Text(
-                    "Name: $nameController",
-                    style: const TextStyle(
-                        fontSize: 18, fontWeight: FontWeight.w500),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    "Email: $emailController",
-                    style: const TextStyle(
-                        fontSize: 18, fontWeight: FontWeight.w500),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    "Phone: $phoneController",
-                    style: const TextStyle(
-                        fontSize: 18, fontWeight: FontWeight.w500),
+                    "Design",
+                    style: TextStyle(
+                      fontSize: 34,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                    ),
                   ),
                 ],
               ),
             ),
-            elevation: 8,
-            shadowColor: Colors.green,
-            margin: const EdgeInsets.all(20),
-            shape: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10),
-              borderSide: const BorderSide(color: Colors.green, width: 1),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.all(48.0),
+                child: Column(
+                  children: [
+                    Text(
+                      "Name: $nameController",
+                      style: const TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.w500),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Email: $emailController",
+                      style: const TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.w500),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Phone: $phoneController",
+                      style: const TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.w500),
+                    ),
+                  ],
+                ),
+              ),
+              elevation: 8,
+              shadowColor: Colors.green,
+              margin: const EdgeInsets.all(20),
+              shape: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: const BorderSide(color: Colors.green, width: 1),
+              ),
             ),
           ),
-        ),
-        const SizedBox(
-          height: 50,
-        ),
-        const CarouselSlider()
-      ],
-    );
-  }
-}
-
-class CarouselSlider extends StatefulWidget {
-  const CarouselSlider(
-      {Key? key, CarouselOptions? options, List<Widget>? items})
-      : super(key: key);
-
-  @override
-  State<CarouselSlider> createState() => _CarouselSliderState();
-}
-
-class _CarouselSliderState extends State<CarouselSlider> {
-  int _current = 0;
-
-  final List<String> images = [
-    "assets/images/ht1.jpeg"
-        "assets/images/ht2.jpeg"
-        "assets/images/ht3.jpeg"
-        "assets/images/ht4.jpeg"
-        "assets/images/ht5.jpeg"
-  ];
-
-  final List<String> names = [
-    "Neuro-link"
-        "JAVES"
-        "Artificial Intelligence"
-        "Archnet"
-        "6G"
-  ];
-
-  List<Widget> generateImageTiles() {
-    return images
-        .map((value) => ClipRRect(
-              child: Image.asset(
-                value,
-                fit: BoxFit.cover,
-              ),
-              borderRadius: BorderRadius.circular(15),
-            ))
-        .toList();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return SizedBox(
-      width: size.width*0.5,
-      child: Stack(
-        children: [
-          CarouselSlider(
-            items: generateImageTiles(),
-            options: CarouselOptions(),
+          const SizedBox(
+            height: 20,
           ),
+          
+          // const CarouselSlider()
         ],
       ),
     );
   }
 }
+
+// var imageTitles = [
+//   "Neuro-link",
+//   "JAVES",
+//   "Artificial Intelligence",
+//   "Archnet",
+//   "6G"
+// ];
+// var carouselImages = [
+//   "assets/images/ht1.jpeg"
+//       "assets/images/ht2.jpeg"
+//       "assets/images/ht3.jpeg"
+//       "assets/images/ht4.jpeg"
+//       "assets/images/ht5.jpeg"
+// ];
+
+// class CarouselSlider extends StatelessWidget {
+//   const CarouselSlider({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     Size size = MediaQuery.of(context).size;
+
+//     PageController controller = PageController(initialPage: 0, viewportFraction: 0.5);
+
+//     List<Widget> banner = <Widget>[];
+
+//     for (int i = 0; i < imageTitles.length; i++) {
+//       var itemView = Stack(
+//         fit: StackFit.expand,
+//         children: [
+//           Image.asset(
+//             carouselImages[i],
+//             fit: BoxFit.cover,
+//           )
+//         ],
+//       );
+//       banner.add(itemView);
+//     }
+//     return SizedBox(
+//       width: size.width,
+//       height: 100,
+//       child: PageView(
+//         controller: controller,
+//         scrollDirection: Axis.horizontal,
+//         children: banner,
+//       ),
+//     );
+//   }
+// }
