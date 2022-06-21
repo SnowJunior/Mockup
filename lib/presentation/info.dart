67243727f3c0f2f601cb87fca70414c6dd1d61d9
Junior <onyangogeorge954@gmail.com>
2022-06-21 10:18:55 +0300

    add form validation and throw back error

--- lib/presentation/info.dart
+++ lib/presentation/info.dart
@@ -10,6 +10,8 @@ class InfoScreen extends StatefulWidget {
   State<InfoScreen> createState() => _InfoScreenState();
 }
 
+final _formKey = GlobalKey<FormState>();
+
 class _InfoScreenState extends State<InfoScreen> {
   late String mValue;
   late String nValue;
@@ -53,127 +55,183 @@ class _InfoScreenState extends State<InfoScreen> {
         ),
         Padding(
           padding: const EdgeInsets.only(left: 18.0, right: 18.0),
-          child: Column(
-            children: [
-              TextFormField(
-                key: const ValueKey(1),
-                onChanged: (_name) {
-                  nValue = _name;
-                },
-                keyboardType: TextInputType.name,
-                decoration: const InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                    borderSide:
-                        BorderSide(color: Colors.greenAccent, width: 1.0),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black, width: 1.0),
-                  ),
-                  label: Text(
-                    "Enter Name",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w300,
-                      color: Colors.black,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  key: const ValueKey(1),
+                  onChanged: (_name) {
+                    nValue = _name;
+                  },
+                  keyboardType: TextInputType.name,
+                  decoration: const InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Colors.greenAccent, width: 1.0),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black, width: 1.0),
+                    ),
+                    label: Text(
+                      "Enter Name",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.black,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                key: const ValueKey(2),
-                onChanged: (_email) {
-                  eValue = _email;
-                },
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                    borderSide:
-                        BorderSide(color: Colors.greenAccent, width: 1.0),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  key: const ValueKey(2),
+                  onChanged: (_email) {
+                    eValue = _email;
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Colors.greenAccent, width: 1.0),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black, width: 1.0),
+                    ),
+                    label: Text(
+                      "Enter Email",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.black,
+                      ),
+                    ),
                   ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black, width: 1.0),
+                  validator: (email) {
+                    email = "onny@gmail.com";
+                    return RegExp(
+                                r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                            .hasMatch(email.trim().toLowerCase())
+                        ? null
+                        : "Enter a valid Email";
+                  },
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  key: const ValueKey(3),
+                  onChanged: (_mobile) {
+                    mValue = _mobile;
+                  },
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly,
+                  ],
+                  decoration: const InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Colors.greenAccent, width: 1.0),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black, width: 1.0),
+                    ),
+                    label: Text(
+                      "Enter Phone Number",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.black,
+                      ),
+                    ),
                   ),
-                  label: Text(
-                    "Enter Email",
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    
+                  },
+                  child: const Text(
+                    "Submit",
                     style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w300,
-                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,
                     ),
                   ),
                 ),
-                validator: (email) {
-                  email = "onny@gmail.com";
-                  return RegExp(
-                              r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                          .hasMatch(email.trim().toLowerCase())
-                      ? null
-                      : "Enter a valid Email";
-                },
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                key: const ValueKey(3),
-                onChanged: (_mobile) {
-                  mValue = _mobile;
-                },
-                keyboardType: TextInputType.number,
-                inputFormatters: <TextInputFormatter>[
-                  FilteringTextInputFormatter.digitsOnly,
-                ],
-                decoration: const InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                    borderSide:
-                        BorderSide(color: Colors.greenAccent, width: 1.0),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black, width: 1.0),
-                  ),
-                  label: Text(
-                    "Enter Phone Number",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w300,
-                      color: Colors.black,
-                    ),
-                  ),
-                ),
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                        builder: (context) => DesignPage(
-                              emailController: eValue,
-                              nameController: nValue,
-                              phoneController: mValue,
-                            )),
-                  );
-                },
-                child: const Text(
-                  "Submit",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ],
     );
+  }
+
+  void submit() {
+    if (_formKey.currentState!.validate() == true) {
+    Navigator.of(context).push(
+        MaterialPageRoute(
+            builder: (context) => DesignPage(
+                  emailController: eValue,
+                  nameController: nValue,
+                  phoneController: mValue,
+                )),
+      ); 
+    } else{
+           ScaffoldMessenger.of(context)
+        ..hideCurrentSnackBar()
+        ..showSnackBar(
+          SnackBar(
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
+              ),
+            ),
+            duration: const Duration(seconds: 2),
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            content: Container(
+              // clipBehavior: Clip.antiAliasWithSaveLayer,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        "Fill all Fields",
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.onPrimary,
+                          fontSize: 16,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                    ],
+                  ),
+                  CircularProgressIndicator.adaptive(
+                    valueColor: AlwaysStoppedAnimation(
+                      Theme.of(context).colorScheme.onPrimary,
+                    ),
+                  )
+                ],
+              ),
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  topRight: Radius.circular(10),
+                ),
+              ),
+            ),
+          ),
+        );
+    }
   }
 }
