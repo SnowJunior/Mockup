71feaa94fee6ad2680764bc80311e45968307857
Junior <onyangogeorge954@gmail.com>
2022-06-29 10:08:00 +0300

    house keeping

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -1,36 +1,37 @@
 // To parse this JSON data, do
 
-
 import 'dart:convert';
 
-List<Todo> todoFromJson(String str) => List<Todo>.from(json.decode(str).map((x) => Todo.fromJson(x)));
+List<Todo> todoFromJson(String str) =>
+    List<Todo>.from(json.decode(str).map((x) => Todo.fromJson(x)));
 
-String todoToJson(List<Todo> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+String todoToJson(List<Todo> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class Todo {
-    Todo({
-        required this.userId,
-        required this.id,
-        required this.title,
-        required this.completed,
-    });
+  Todo({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.completed,
+  });
 
-    int userId;
-    int id;
-    String title;
-    bool completed;
+  int userId;
+  int id;
+  String title;
+  bool completed;
 
-    factory Todo.fromJson(Map<String, dynamic> json) => Todo(
+  factory Todo.fromJson(Map<String, dynamic> json) => Todo(
         userId: json["userId"],
         id: json["id"],
         title: json["title"],
         completed: json["completed"],
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "userId": userId,
         "id": id,
         "title": title,
         "completed": completed,
-    };
+      };
 }
